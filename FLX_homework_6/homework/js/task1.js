828849d78f992110d36d73345e3902c3684ba207
Yuliia Liench <julia.liashenko03@gmail.com>
2019-02-05 21:14:21 +0300

    homework6

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,3 +1,57 @@
-// Your code goes here
+function discriminant(a, b, c) {
+    return Math.pow(b, 2) - 4 * a * c;
+}
 
-console.log('dsds');
+function getX(a, b, D, sign) {
+    var multiplier = sign ? 1 : -1;
+
+   return (-b + multiplier * Math.sqrt(D)) / (2 * a);
+}
+
+function isValidNumber (n) {
+    return !isNaN(n) && isFinite(n);
+}
+
+function validate(a, b, c) {
+    if (!isValidNumber(a)) {
+        return false;
+    }
+    if (!isValidNumber(b)) {
+        return false;
+    }
+    if (!isValidNumber(c)) {
+        return false;
+    }
+    if (a === 0) {
+        return false;
+    }            
+    return true;   
+}  
+
+function quadraticEquation(a, b, c) {
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+    
+    if (!validate(a, b, c)){
+        return "Invalid input data";
+    }
+    var d = discriminant(a, b, c);
+    
+    if (d < 0) {
+        return 'no solution';
+    } else if (d === 0) {
+        var x = getX(a, b, d) 
+        return "x = " + x;
+    } else {      
+        var x1 = getX(a, b, d, true);
+        var x2 = getX(a, b, d, false);
+        return "x1 = " + x1 + " and x2 = " + x2;
+    }        
+}
+
+var a = prompt("a =");
+var b = prompt("b =");
+var c = prompt("c =");
+var result = quadraticEquation(a, b, c);
+alert (result);
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,1 +1,37 @@
-// Your code goes here
+function calculateDiscount(price, percentDiscount){
+    price = Number(price);
+    percentDiscount = Number(percentDiscount);
+    if (!validate(price, percentDiscount)) {
+        return "Invalid input data";
+    }
+    var discount = price * percentDiscount / 100;
+    discount = + discount.toFixed(2);
+    var priceWithDiscount = price - discount;
+    return "Price without discount: " + price + "\n\r Discount: " + percentDiscount + "%" + 
+    "\n\r Price with discount: " + priceWithDiscount + "\n\r Saved: " + discount;
+}
+
+function isValidNumber (n) {
+    return !isNaN(n) && isFinite(n);
+}
+
+function validate(price, percentDiscount) {
+    if (!isValidNumber(price)) {
+        return false;
+    }
+    if (!isValidNumber(percentDiscount)) {
+        return false;
+    }
+    if (price < 0 || price > 9999999) {
+        return false;
+    }
+    if (percentDiscount < 0 || percentDiscount > 99) {
+        return false;
+    }
+    return true; 
+}
+
+var price = prompt("input amount of money");
+var percentDiscount = prompt("input discount");
+var result = calculateDiscount(price, percentDiscount);
+alert (result);
